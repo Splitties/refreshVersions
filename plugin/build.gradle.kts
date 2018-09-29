6045512c30b0dc45be733d467fc88ece73945706
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2018-09-29 07:15:13 +0200

    Initialized from kotlin-dsl/samples/plugin

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    `maven-publish`
+}
+
+group = "my"
+version = "1.0"
+
+gradlePlugin {
+    plugins {
+        register("myPlugin") {
+            id = "my-plugin"
+            implementationClass = "plugin.MyPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven(url = "build/repository")
+    }
+}
+
+repositories {
+    jcenter()
+}
