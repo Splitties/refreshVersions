829ccce8d36f39119ca5e0a932eb1220145a1b84
Jean-Michel Fayard <jean-michel.fayard@akelius.de>
2019-08-21 13:08:40 +0200

    Revert "plugin module now called buildSrcVersions-plugin"
    This reverts commit 80f901f3

--- composite/build.gradle.kts
+++ composite/build.gradle.kts
@@ -7,23 +7,23 @@ group = "de.fayard"
 version = "0.4.0"
 defaultTasks("run")
 
+
 tasks.register("publishLocally") {
     group = "Custom"
     description = "Publish the plugin locally"
-    dependsOn(gradle.includedBuild("buildSrcVersions-plugin").task(":publish"))
+    dependsOn(gradle.includedBuild("buildSrcVersions").task(":publish"))
 }
-
 tasks.register("publishPlugins") {
     group = "Custom"
     description = "Publishes this plugin to the Gradle Plugin portal."
-    dependsOn(gradle.includedBuild("buildSrcVersions-plugin").task(":publish"))
+    dependsOn(gradle.includedBuild("buildSrcVersions").task(":publish"))
 }
 
 tasks.register("checkAll") {
     group = "Custom"
     description = "Run all checks"
-    dependsOn(gradle.includedBuild("buildSrcVersions-plugin").task(":validateTaskProperties"))
-    dependsOn(gradle.includedBuild("buildSrcVersions-plugin").task(":check"))
+    dependsOn(gradle.includedBuild("buildSrcVersions").task(":validateTaskProperties"))
+    dependsOn(gradle.includedBuild("buildSrcVersions").task(":check"))
     dependsOn(gradle.includedBuild("sample-kotlin").task(":buildSrcVersions"))
     dependsOn(gradle.includedBuild("sample-groovy").task(":buildSrcVersions"))
 }
