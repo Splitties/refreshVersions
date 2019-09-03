26a5ed24fc51a69a464e8bcc7a1fc4f2eec62f94
Jean-Michel Fayard <jean-michel.fayard@akelius.de>
2019-09-03 18:33:41 +0200

    #55 Implement GROOVY_DEF && GROOVY_EXT

--- plugin/src/main/kotlin/de/fayard/VersionsOnlyMode.kt
+++ plugin/src/main/kotlin/de/fayard/VersionsOnlyMode.kt
@@ -4,29 +4,12 @@ import java.io.File
 
 fun BuildSrcVersionsExtension.useBuildSrc() = versionsOnlyMode != null
 
-enum class VersionsOnlyMode(val example: String) {
-    KOTLIN_VAL("""
-        val okhttp = "3.2.0" // available: "3.3.1"
-        val com_squareup_moshi = "1.8.0"
-    """.trimIndent()),
-    KOTLIN_OBJECT("""
-    object {
-        val okhttp = "3.2.0" // available: "3.3.1"
-        val com_squareup_moshi = "1.8.0"
-    }         
-    """.trimIndent()),
-    GROOVY_DEF("""
-        def okhttp = '3.2.0' // available: '3.3.1'
-        def com_squareup_moshi = '1.8.0'
-    """.trimIndent()),
-    GROOVY_EXT("""
-        ext {
-            okhttp = '3.2.0' // available: '3.3.1'
-            com_squareup_moshi = '1.8.0'
-        }
-    """.trimIndent())
-    ;
-    companion object
+enum class VersionsOnlyMode {
+    KOTLIN_VAL,
+    KOTLIN_OBJECT,
+    GROOVY_DEF,
+    GROOVY_EXT;
+
 }
 
 
@@ -54,23 +37,29 @@ fun parseBuildFile(versionsOnlyFile : File?): SingleModeResult? {
 }
 
 fun regenerateBuildFile(versionsOnlyFile: File?, extension: BuildSrcVersionsExtension, dependencies: List<Dependency>) {
+    val versionsOnlyMode = extension.versionsOnlyMode ?: return
     val (startOfBlock, endOfBlock, indent) = parseBuildFile(versionsOnlyFile) ?: return
     checkNotNull(versionsOnlyFile)
     val lines = versionsOnlyFile.readLines()
 
-    val newBlock = regenerateBlock(extension, dependencies, indent)
+    val newBlock = regenerateBlock(versionsOnlyMode, dependencies, indent)
     val newLines = lines.subList(0, startOfBlock) + newBlock + lines.subList(endOfBlock + 1, lines.size)
     versionsOnlyFile.writeText(newLines.joinToString(separator = "\n", postfix = "\n"))
 }
 
-fun regenerateBlock(extension: BuildSrcVersionsExtension, dependencies: List<Dependency>, indent: String): List<String> {
+fun regenerateBlock(mode: VersionsOnlyMode, dependencies: List<Dependency>, indent: String): List<String> {
     val result = mutableListOf<String>()
     result += PluginConfig.VERSIONS_ONLY_INTRO.map { "$indent// $it" }
-    result += dependencies.map { versionOnly(it, extension, indent) }
+    if (mode == VersionsOnlyMode.GROOVY_EXT) result += "${indent}ext {"
+    result += dependencies.map { versionOnly(it, mode, indent) }
+    if (mode == VersionsOnlyMode.GROOVY_EXT) result += "${indent}}"
     result += "$indent// ${PluginConfig.VERSIONS_ONLY_END}"
     return result
 }
 
-fun versionOnly(d: Dependency, extension: BuildSrcVersionsExtension, indent: String): String {
-    return """${indent}val ${d.versionName} = "${d.version}"${d.versionInformation()}""".trimEnd()
+fun versionOnly(d: Dependency, versionsOnlyMode: VersionsOnlyMode, indent: String): String = when(versionsOnlyMode) {
+    VersionsOnlyMode.KOTLIN_VAL -> """${indent}val ${d.versionName} = "${d.version}"${d.versionInformation()}"""
+    VersionsOnlyMode.GROOVY_DEF -> """${indent}def ${d.versionName} = "${d.version}"${d.versionInformation()}"""
+    VersionsOnlyMode.GROOVY_EXT -> """${indent}${indent}${d.versionName} = "${d.version}"${d.versionInformation()}"""
+    else -> TODO("Not implemented $versionsOnlyMode")
 }
--- plugin/src/test/kotlin/de/fayard/VersionsOnlyModeTest.kt
+++ plugin/src/test/kotlin/de/fayard/VersionsOnlyModeTest.kt
@@ -7,25 +7,35 @@ class VersionsOnlyModeTest: FreeSpec({
 
     val samFolder = testResourceFile("sam")
 
-    val kotlinValInput = samFolder.resolve("KOTLIN_VAL_input.txt")
-    val kotlinValReceived = samFolder.resolve("KOTLIN_VAL_received.txt")
-    val kotlinValValidated = samFolder.resolve("KOTLIN_VAL_validated.txt")
+    val kotlinValInput = samFolder.resolve("singleBuildFile.txt")
+
 
     "Parse file" {
         val singleModeResult = parseBuildFile(kotlinValInput)
         singleModeResult shouldBe SingleModeResult(6, 11, "    ")
     }
 
-    "Regenerate file" {
-        kotlinValInput.copyTo(kotlinValReceived, overwrite = true)
-        val config = BuildSrcVersionsExtensionImpl(versionsOnlyMode = VersionsOnlyMode.KOTLIN_VAL, versionsOnlyFile = "KOTLIN_VAL_expected.txt")
-        val deps = listOf(
-            "com.squareup.okhttp3:okhttp:2.1.0 // 2.2.0",
-            "com.squareup.okio:okio:2.0.0"
-        ).map { it.asDependency() }
-        regenerateBuildFile(kotlinValReceived, config, deps)
-        kotlinValReceived.readText() shouldBe kotlinValValidated.readText()
-        kotlinValReceived.delete()
+    "Regenerate file" - {
+        val testCases = listOf(
+            VersionsOnlyMode.KOTLIN_VAL,
+            VersionsOnlyMode.GROOVY_DEF,
+            VersionsOnlyMode.GROOVY_EXT
+        )
+        for (mode in testCases) {
+            "For mode = $mode" {
+                val received = samFolder.resolve("${mode}_received.txt")
+                val validated = samFolder.resolve("${mode}_validated.txt")
+                kotlinValInput.copyTo(received, overwrite = true)
+                val config = BuildSrcVersionsExtensionImpl(versionsOnlyMode = mode, versionsOnlyFile = "KOTLIN_VAL_expected.txt")
+                val deps = listOf(
+                    "com.squareup.okhttp3:okhttp:2.1.0 // 2.2.0",
+                    "com.squareup.okio:okio:2.0.0"
+                ).map { it.asDependency() }
+                regenerateBuildFile(received, config, deps)
+                received.readText() shouldBe validated.readText()
+                received.delete()
+            }
+        }
     }
 })
 
