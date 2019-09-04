3d9bf6ee16201204df2518105e3515fc4df3e991
Jean-Michel Fayard <jean-michel.fayard@akelius.de>
2019-09-04 15:33:31 +0200

    Included build in /Users/jmfayard/akelius/buildSrcVersions/plugin has the same root project name 'buildSrcVersions-plugin' as the main build.

--- composite/build.gradle.kts
+++ composite/build.gradle.kts
@@ -7,35 +7,38 @@ group = "de.fayard"
 version = "0.4.3"
 defaultTasks("run")
 
+val PLUGIN: IncludedBuild = gradle.includedBuild("buildSrcVersions-plugin")
+val SAMPLE_KOTLIN: IncludedBuild = gradle.includedBuild("sample-kotlin")
+val SAMPLE_GROOVY: IncludedBuild = gradle.includedBuild("sample-groovy")
+val SAMPLE_VERSIONS_ONLY: IncludedBuild = gradle.includedBuild("sample-versionsOnlyMode")
 
 tasks.register("publishLocally") {
     group = "Custom"
     description = "Publish the plugin locally"
-    dependsOn(gradle.includedBuild("buildSrcVersions").task(":publish"))
+    dependsOn(PLUGIN.task(":publish"))
 }
+
 tasks.register("publishPlugins") {
     group = "Custom"
     description = "Publishes this plugin to the Gradle Plugin portal."
-    dependsOn(gradle.includedBuild("buildSrcVersions").task(":publishPlugins"))
+    dependsOn(PLUGIN.task(":publishPlugins"))
 }
+
 tasks.register("pluginTests") {
     group = "Custom"
     description = "Run plugin unit tests"
-    dependsOn(gradle.includedBuild("buildSrcVersions").task(":check"))
+    dependsOn(PLUGIN.task(":check"))
 }
+
 tasks.register("checkAll") {
     group = "Custom"
     description = "Run all checks"
-    dependsOn(gradle.includedBuild("buildSrcVersions").task(":validateTaskProperties"))
-    dependsOn(gradle.includedBuild("buildSrcVersions").task(":check"))
-    dependsOn(gradle.includedBuild("sample-kotlin").task(":buildSrcVersions"))
-    dependsOn(gradle.includedBuild("sample-groovy").task(":buildSrcVersions"))
-    dependsOn(gradle.includedBuild("sample-versionsOnlyMode").task(":buildSrcVersions"))
+    dependsOn(PLUGIN.task(":validateTaskProperties"))
+    dependsOn(PLUGIN.task(":check"))
+    dependsOn(SAMPLE_KOTLIN.task(":buildSrcVersions"))
+    dependsOn(SAMPLE_GROOVY.task(":buildSrcVersions"))
+    dependsOn(SAMPLE_VERSIONS_ONLY.task(":buildSrcVersions"))
 }
-
-
-
-
 
 buildScan {
     termsOfServiceUrl = "https://gradle.com/terms-of-service"
