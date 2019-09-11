76dce4a6e4aa102725eb8ab6b69a6397abaf73bd
Jean-Michel Fayard <jean-michel.fayard@akelius.de>
2019-09-11 23:58:54 +0200

    #60 configure plugin version from gradle.properties

--- sample-kotlin/settings.gradle.kts
+++ sample-kotlin/settings.gradle.kts
@@ -3,6 +3,12 @@ pluginManagement {
         mavenLocal()
         gradlePluginPortal()
     }
+    resolutionStrategy.eachPlugin {
+        val key = "plugin.${requested.id.id}"
+        if (extra.has(key)) {
+            useVersion(extra.get(key) as String)
+        }
+    }
 }
 rootProject.name = "sample-kotlin"
 includeBuild("../plugin")
