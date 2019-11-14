d37c1b1e69cfc80a2ad6a501257fe8c41917e128
Louis CAD <louis.cognault@gmail.com>
2019-11-14 17:15:02 +0100

    Lower functions visibility

--- refreshVersions/src/main/kotlin/de/fayard/versions/LatestVersionRetrieval.kt
+++ refreshVersions/src/main/kotlin/de/fayard/versions/LatestVersionRetrieval.kt
@@ -14,20 +14,22 @@ internal fun Project.getLatestDependencyVersionFromRepo(
 ): String? {
     val mavenMetadataUris = getMavenMetadataUrls(dependency)
     val versions = mavenMetadataUris.asSequence().mapNotNull { uri ->
-        runCatching { uri.toURL().readText() }.getOrNull()?.let { xml ->
+        runCatching {
+            uri.toURL().readText()
+        }.getOrNull()?.let { xml ->
             parseVersionsFromMavenMetaData(xml)
         }
     }.flatten().sortedWith(versionComparator).distinct()
     return versions.last().value
 }
 
-internal fun parseVersionsFromMavenMetaData(xml: String): List<Version> {
+private fun parseVersionsFromMavenMetaData(xml: String): List<Version> {
     return xml.substringAfter("<versions>").substringBefore("</versions>")
         .split("<version>", "</version>")
         .mapNotNull { if (it.isBlank()) null else Version(it.trim()) }
 }
 
-internal fun Project.getMavenMetadataUrls(dependency: Dependency): List<URI> {
+private fun Project.getMavenMetadataUrls(dependency: Dependency): List<URI> {
     val group = dependency.group ?: return emptyList()
     val urlEnd = "${group.replace('.', '/')}/${dependency.name}/maven-metadata.xml"
     return repositories.mapNotNull { (it as? MavenArtifactRepository)?.url?.resolve(urlEnd) }.let {
