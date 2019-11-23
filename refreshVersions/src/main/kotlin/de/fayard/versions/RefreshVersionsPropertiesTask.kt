896bd1ced71effb855fc0d43f8aea6eed18646bc
Louis CAD <louis.cognault@gmail.com>
2019-11-23 19:05:42 +0100

    Make latest versions retrieval parallel
    Uses kotlinx.coroutines to make it easy.
    
    That brings a huge performance increase:
    instead of talking minutes, we talk seconds, so about 60 times faster
    for a test project.

--- refreshVersions/src/main/kotlin/de/fayard/versions/RefreshVersionsPropertiesTask.kt
+++ refreshVersions/src/main/kotlin/de/fayard/versions/RefreshVersionsPropertiesTask.kt
@@ -2,6 +2,9 @@ package de.fayard.versions
 
 import de.fayard.versions.extensions.isGradlePlugin
 import de.fayard.versions.extensions.moduleIdentifier
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
@@ -46,31 +49,33 @@ open class RefreshVersionsPropertiesTask : DefaultTask() {
 
         val versionProperties: Map<String, String> = project.getVersionProperties()
 
-        val dependenciesWithLastVersion: List<Pair<Dependency, List<VersionCandidate>>>
-        dependenciesWithLastVersion = allDependencies.mapNotNull { dependency ->
+        val dependenciesWithVersionCandidates: List<Pair<Dependency, List<VersionCandidate>>> = runBlocking {
+            allDependencies.mapNotNull { dependency ->
 
-            println("Dependency ${dependency.group}:${dependency.name}:${dependency.version}")
-            //TODO: Replace line above with optional diagnostic option, or show status in progress.
+                println("Dependency ${dependency.group}:${dependency.name}:${dependency.version}")
+                //TODO: Replace line above with optional diagnostic option, or show status in progress.
 
-            if (dependency.isManageableVersion(versionProperties).not()) {
-                return@mapNotNull null //TODO: Keep aside to report hardcoded versions and version ranges,
-                //todo... see this issue: https://github.com/jmfayard/buildSrcVersions/issues/126
-            }
-
-            val versionCandidates = project.rootProject.getDependencyVersionsCandidates(
-                extension = extension,
-                repositories = allRepositories,
-                group = dependency.group ?: return@mapNotNull null,
-                name = dependency.name,
-                resolvedVersion = resolveVersion(
+                if (dependency.isManageableVersion(versionProperties).not()) {
+                    return@mapNotNull null //TODO: Keep aside to report hardcoded versions and version ranges,
+                    //todo... see this issue: https://github.com/jmfayard/buildSrcVersions/issues/126
+                }
+                val group = dependency.group ?: return@mapNotNull null
+                val resolvedVersion = resolveVersion(
                     properties = versionProperties,
                     key = dependency.moduleIdentifier?.getVersionPropertyName() ?: return@mapNotNull null
                 )
-            )
-
-            return@mapNotNull dependency to versionCandidates
-        }.toList()
-        project.rootProject.updateVersionsProperties(dependenciesWithLastVersion)
+                async {
+                    dependency to getDependencyVersionsCandidates(
+                        extension = extension,
+                        repositories = allRepositories,
+                        group = group,
+                        name = dependency.name,
+                        resolvedVersion = resolvedVersion
+                    )
+                }
+            }.toList().awaitAll()
+        }
+        project.rootProject.updateVersionsProperties(dependenciesWithVersionCandidates)
     }
 
     private fun Dependency.isManageableVersion(versionProperties: Map<String, String>): Boolean {
