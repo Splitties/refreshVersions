a5b70e22e55f773d47955f2b7d415c21493b04d7
Louis CAD <louis.cognault@gmail.com>
2019-11-24 00:13:29 +0100

    Version 0.8.5

--- sample-kotlin/settings.gradle.kts
+++ sample-kotlin/settings.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
         gradlePluginPortal()
         mavenCentral()
     }
-    dependencies.classpath("de.fayard.refreshVersions:de.fayard.refreshVersions.gradle.plugin:0.8.4")
+    dependencies.classpath("de.fayard.refreshVersions:de.fayard.refreshVersions.gradle.plugin:0.8.5")
 }
 
 setupVersionPlaceholdersResolving()
