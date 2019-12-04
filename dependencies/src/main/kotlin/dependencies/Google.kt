2d0f7e4c6fa01dd98d67171acc02a2ac748c4aa5
Louis CAD <louis.cognault@gmail.com>
2019-12-04 14:21:34 +0100

    Add missing Google Play Services constants

--- dependencies/src/main/kotlin/dependencies/Google.kt
+++ dependencies/src/main/kotlin/dependencies/Google.kt
@@ -22,19 +22,65 @@ object Google {
         object PlayServices {
             private const val artifactPrefix = "$artifactBase.gms:play-services"
 
+            /** Google Account Login */
+            const val auth = "$artifactPrefix-auth:_"
+
+            /** Base client library and Google Actions */
             const val base = "$artifactPrefix-base:_"
 
-            const val auth = "$artifactPrefix-auth:_"
+            /** Google Sign In */
+            const val identity = "$artifactPrefix-identity:_"
+
+            /** Google Analytics */
+            const val analytics = "$artifactPrefix-analytics:_"
+
+            /** Google Awareness */
+            const val awareness = "$artifactPrefix-awareness:_"
+
+            /** Google Cast */
+            const val cast = "$artifactPrefix-cast:_"
+
+            /** Google Cloud Messaging */
+            @Deprecated("Use Firebase Cloud Messaging instead")
+            const val gcm = "$artifactPrefix-gcm:_"
+
+            /** Google Drive */
+            const val drive = "$artifactPrefix-drive:_"
+
+            /** Google Fit */
+            const val fitness = "$artifactPrefix-fitness:_"
+
+            /** Google Location and Activity Recognition */
             const val location = "$artifactPrefix-location:_"
 
+            // Play Services Ads intentionally not included because ads are mental pollution.
+
+            /**
+             * Google Tasks API (yet another "Future" type, because they're not using Kotlin coroutines yet).
+             * We recommend to use it with [KotlinX.Coroutines.playServices].
+             */
             const val tasks = "$artifactPrefix-tasks:_"
 
-            private const val visionVersion = "_"
+            /** Mobile Vision */
+            const val vision = "$artifactPrefix-vision:_"
 
-            const val vision = "$artifactPrefix-vision:$visionVersion"
-            const val visionCommon = "$artifactPrefix-vision-common:$visionVersion"
+            /** Google Nearby */
+            const val nearby = "$artifactPrefix-nearby:_"
 
-            const val wearable = "$artifactPrefix-wearable:_"
+            /** Google Panorama Viewer */
+            const val panorama = "$artifactPrefix-panorama:_"
+
+            /** Google Play Game services */
+            const val games = "$artifactPrefix-games:_"
+
+            /** SafetyNet */
+            const val safetynet = "$artifactPrefix-safetynet:_"
+
+            /** Google Pay */
+            const val pay = "$artifactPrefix-wallet:_"
+
+            /** Wear OS by Google */
+            const val wearOS = "$artifactPrefix-wearable:_"
         }
     }
 }
