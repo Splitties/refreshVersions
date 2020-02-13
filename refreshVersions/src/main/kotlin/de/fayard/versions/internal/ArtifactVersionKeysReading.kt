e77fcd05c8ae8953ecdd260a0eab383d1f47aa15
Louis CAD <louis.cognault@gmail.com>
2020-02-13 14:03:00 +0100

    Implement parser for artifact version key rules

--- refreshVersions/src/main/kotlin/de/fayard/versions/internal/ArtifactVersionKeysReading.kt
+++ refreshVersions/src/main/kotlin/de/fayard/versions/internal/ArtifactVersionKeysReading.kt
@@ -4,9 +4,21 @@
 
 package de.fayard.versions.internal
 
-internal fun parseArtifactVersionKeysRules(fileContent: String): Nothing {
-    TODO("Check file, and report any errors by throwing")
-    TODO("split impl")
+internal fun parseArtifactVersionKeysRules(fileContent: String): List<ArtifactVersionKeyRule> {
+    val minimized = fileContent
+        .replace("//.*$".toRegex(), "") // Remove line comments
+        .replace("\\s*\$".toRegex(), "") // Remove trailing whitespace
+    //.replace("^.*$".toRegex(), "") // Remove empty lines
+    val lines = minimized.lines()
+    require(lines.size % 2 == 0) {
+        "Every artifact version key rule is made of two lines, but an odd count of rules lines has been found."
+    }
+    return List(lines.size / 2) { i ->
+        ArtifactVersionKeyRule(
+            artifactPattern = lines[i * 2],
+            versionKeyPattern = lines[i * 2 + 1]
+        )
+    }
 }
 
 private fun checkArtifactVersionKeysRulesFiles(fileContent: String) {
