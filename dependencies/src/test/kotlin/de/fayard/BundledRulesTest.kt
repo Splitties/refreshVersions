5cd906e7c9fa84374cf39817e6d79d2ad6a22e23
Louis CAD <louis.cognault@gmail.com>
2020-02-13 14:56:36 +0100

    Disable not implemented test

--- dependencies/src/test/kotlin/de/fayard/BundledRulesTest.kt
+++ dependencies/src/test/kotlin/de/fayard/BundledRulesTest.kt
@@ -1,10 +1,12 @@
 package de.fayard
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class BundledRulesTest {
 
     @Test
+    @Disabled("Not implemented yet")
     fun testKotlinxRules() {
         TODO()
     }
