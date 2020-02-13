14cc4dbb2eb6a513d5f9b7bfa64ef491736d740a
Louis CAD <louis.cognault@gmail.com>
2020-02-13 17:50:40 +0100

    Rename resources to testResources in tests

--- dependencies/src/test/kotlin/de/fayard/NonRegression.kt
+++ dependencies/src/test/kotlin/de/fayard/NonRegression.kt
@@ -8,13 +8,13 @@ import io.kotlintest.should
 import io.kotlintest.specs.FreeSpec
 import java.io.File
 
-val resources: File = File(".").absoluteFile.resolve("src/test/resources")
+val testResources: File = File(".").absoluteFile.resolve("src/test/resources")
 
 class NonRegression: FreeSpec({
 
     "We should never remove a property" {
-        val existingProperties = resources.resolve("dependencies-mapping-validated.txt")
-        val receivedProperties = resources.resolve("dependencies-mapping-received.txt")
+        val existingProperties = testResources.resolve("dependencies-mapping-validated.txt")
+        val receivedProperties = testResources.resolve("dependencies-mapping-received.txt")
 
         val existingMapping = existingProperties.readLines().mapNotNull { DependencyMapping.fromLine(it) }
         val receivedMapping = getArtifactNameToConstantMapping()
