9ad710bac7e46c4612a45f321951c5b9223b2325
Louis CAD <louis.cognault@gmail.com>
2020-02-17 02:39:37 +0100

    Fix version of dependencies plugin

--- dependencies/settings.gradle.kts
+++ dependencies/settings.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         gradlePluginPortal()
         mavenCentral()
     }
-    dependencies.classpath("de.fayard:refreshVersions:0.8.7")
+    dependencies.classpath("de.fayard:refreshVersions:0.9.0")
 }
 
 setupVersionPlaceholdersResolving()
