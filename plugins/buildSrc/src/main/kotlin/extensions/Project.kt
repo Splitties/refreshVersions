911b7347c67ab07c610085e8e9f39140c5f6726e
Louis CAD <louis.cognault@gmail.com>
2020-06-01 14:54:01 +0200

    Make Project.propertyOrEnv nullable

--- plugins/buildSrc/src/main/kotlin/extensions/Project.kt
+++ plugins/buildSrc/src/main/kotlin/extensions/Project.kt
@@ -2,6 +2,6 @@ package extensions
 
 import org.gradle.api.Project
 
-fun Project.propertyOrEnv(key: String): String {
+fun Project.propertyOrEnv(key: String): String? {
     return findProperty(key) as String? ?: System.getenv(key)
 }
