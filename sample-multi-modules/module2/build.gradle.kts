797c76aa004b8987129a6e0d9c52d1debe6d880a
Louis CAD <louis.cognault@gmail.com>
2020-07-08 14:41:31 +0200

    Add more dependencies to sample-multi-module
    This makes it more intensive and allows to spot performance issues, if any.

--- sample-multi-modules/module2/build.gradle.kts
+++ sample-multi-modules/module2/build.gradle.kts
@@ -16,12 +16,54 @@ repositories {
     }*/
     maven("https://dl.bintray.com/kotlin/kotlin-eap")
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(Square.retrofit2.retrofit)
+    implementation(KotlinX.coroutines.core)
+
     //implementation("se.jsimo.hello.maven:hello-world-maven:1.0.4-9-g716f2e1")
     //implementation("se.jsimo.hello.maven:hello-world-maven:_")
+
+    implementation(Ktor.client.auth)
+    implementation(Ktor.client.authBasic)
+    implementation(Ktor.client.core)
+    implementation(Ktor.client.cio)
+    implementation(Ktor.client.json)
+    implementation(Ktor.client.okHttp)
+    implementation(Ktor.client.serialization)
+    implementation(Ktor.client.logging)
+    implementation(Ktor.client.encoding)
+    implementation(Ktor.client.websockets)
+    implementation(Ktor.http)
+    implementation(Ktor.features.htmlBuilder)
+    implementation(Ktor.features.auth)
+    implementation(Ktor.network.network)
+    implementation(Ktor.network.tls)
+    implementation(Ktor.server.core)
+    implementation(Ktor.utils)
+
+    testImplementation(Testing.junit.api)
+    testImplementation(Testing.junit.engine)
+    testImplementation(Testing.junit.junitJupiter)
+    testImplementation(Testing.junit.params)
+    testImplementation(Testing.kotlinTest.assertions.assertions)
+    testImplementation(Testing.kotlinTest.assertions.ktor)
+    testImplementation(Testing.kotlinTest.core)
+    testImplementation(Testing.kotlinTest.extensions.extensions)
+    testImplementation(Testing.kotlinTest.plugins.piTest)
+    testImplementation(Testing.kotlinTest.runner.junit5)
+    testImplementation(Testing.mockK.common)
+    testImplementation(Testing.mockK.mockK)
+    testImplementation(Testing.mockito.core)
+    testImplementation(Testing.mockito.errorProne)
+    testImplementation(Testing.mockito.junitJupiter)
+    testImplementation(Testing.mockito.kotlin)
+    testImplementation(Testing.mockito.inline)
+
     testImplementation("junit", "junit", "4.12")
 }
+// Engie 44
+//IBKSPLUS58606
