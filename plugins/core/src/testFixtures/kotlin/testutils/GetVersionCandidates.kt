1b60af52251f55f1f7eea294ccdacbe948b03bec
Louis CAD <louis.cognault@gmail.com>
2020-07-18 06:32:26 +0200

    Suppress false positive invisible_reference errors
    Must be reverted when https://youtrack.jetbrains.com/issue/KT-34102 is fixed.

--- plugins/core/src/testFixtures/kotlin/testutils/GetVersionCandidates.kt
+++ plugins/core/src/testFixtures/kotlin/testutils/GetVersionCandidates.kt
@@ -1,3 +1,5 @@
+@file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER") //TODO: Remove when https://youtrack.jetbrains.com/issue/KT-34102 is fixed.
+
 package testutils
 
 import de.fayard.refreshVersions.core.ModuleId
@@ -6,9 +8,6 @@ import de.fayard.refreshVersions.core.internal.MavenDependencyVersionsFetcher
 import de.fayard.refreshVersions.core.internal.VersionCandidatesResultMode
 import de.fayard.refreshVersions.core.internal.getVersionCandidates
 import okhttp3.OkHttpClient
-
-// NOTE: Errors about symbols being internal can safely be ignored here, it's an IDE issue.
-//TODO: Remove comment above when https://youtrack.jetbrains.com/issue/KT-34102 is fixed.
 
 suspend fun getVersionCandidates(
     httpClient: OkHttpClient,
