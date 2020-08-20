1ee7938702349800a25c61a2285bad009259e7f0
Louis CAD <louis.cognault@gmail.com>
2020-08-20 16:53:33 +0200

    Add KDoc to JakeWharton dependencies constants

--- plugins/dependencies/src/main/kotlin/dependencies/JakeWharton.kt
+++ plugins/dependencies/src/main/kotlin/dependencies/JakeWharton.kt
@@ -8,10 +8,21 @@ import org.gradle.api.Incubating
 
 @Incubating
 object JakeWharton {
+
+    /**
+     * A logger with a small, extensible API which provides utility on top of Android's normal Log class.
+     *
+     * GitHub page: [JakeWharton/timber](https://github.com/JakeWharton/timber)
+     */
     const val timber = "com.jakewharton.timber:timber:_"
 
     val retrofit2 = Retrofit2
 
+    /**
+     * A Kotlin DSL and Java/Kotlin builder API for constructing HTML-like tables which can be rendered to text.
+     *
+     * GitHub page: [JakeWharton/picnic](https://github.com/JakeWharton/picnic)
+     */
     const val picnic = "com.jakewharton.picnic:picnic:_"
 
     object Retrofit2 {
@@ -22,6 +33,8 @@ object JakeWharton {
         object Converter {
 
             /**
+             * A Retrofit 2 `Converter.Factory` for [Kotlin serialization](https://github.com/Kotlin/kotlinx.serialization/).
+             *
              * GitHub page: [JakeWharton/retrofit2-kotlinx-serialization-converter](https://github.com/JakeWharton/retrofit2-kotlinx-serialization-converter)
              */
             const val kotlinxSerialization = "$artifactPrefix-kotlinx-serialization-converter:_"
