9beb76612b7c2586e215a2b7e618c3fad6873c02
Louis CAD <louis.cognault@gmail.com>
2020-08-20 18:30:11 +0200

    Add CashApp.copper

--- plugins/dependencies/src/main/kotlin/dependencies/CashApp.kt
+++ plugins/dependencies/src/main/kotlin/dependencies/CashApp.kt
@@ -8,4 +8,21 @@ object CashApp {
      * GitHub page: [cashapp/turbine](https://github.com/cashapp/turbine)
      */
     const val turbine = "app.cash.turbine:turbine:_"
+
+    /**
+     * A content provider wrapper for reactive queries with Kotlin coroutines `Flow` or RxJava `Observable`.
+     *
+     * (Content Provider is an Android API)
+     *
+     * GitHub page: [cashapp/copper](https://github.com/cashapp/copper)
+     */
+    val copper = Copper
+
+    object Copper {
+        private const val artifactPrefix = "app.cash.copper:copper"
+
+        const val flow = "$artifactPrefix-flow:_"
+        const val rx2 = "$artifactPrefix-rx2:_"
+        const val rx3 = "$artifactPrefix-rx3:_"
+    }
 }
