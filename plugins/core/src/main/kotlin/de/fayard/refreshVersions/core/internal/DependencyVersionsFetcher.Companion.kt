7460478446d81c0a96eca185673816a3e22dbd8f
Louis CAD <louis.cognault@gmail.com>
2020-08-21 11:48:36 +0200

    Don't fail for repos that have non password credentials or are not https or file protocols.
    Fixes #215 and closes #216

--- plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/DependencyVersionsFetcher.Companion.kt
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/DependencyVersionsFetcher.Companion.kt
@@ -2,6 +2,7 @@ package de.fayard.refreshVersions.core.internal
 
 import de.fayard.refreshVersions.core.DependencyVersionsFetcher
 import de.fayard.refreshVersions.core.ModuleId
+import de.fayard.refreshVersions.core.extensions.gradle.passwordCredentials
 import okhttp3.Credentials
 import okhttp3.OkHttpClient
 import org.gradle.api.artifacts.Dependency
@@ -14,6 +15,15 @@ internal operator fun DependencyVersionsFetcher.Companion.invoke(
 ): DependencyVersionsFetcher? {
     val group = dependency.group ?: return null // TODO: Support NPM dependencies from Kotlin/JS
     val name = dependency.name
+    when (repository.url.scheme) {
+        "https", "file" -> Unit // Supported
+        "http" -> return null //TODO: Show non fatal error that http is not supported for security reasons.
+        else -> return null //TODO: Support more transport protocols. Here's what Gradle supports:
+        // https://docs.gradle.org/current/userguide/declaring_repositories.html#sec:supported_transport_protocols
+        // We should trigger a warning that it's not supported yet, with link to relevant issue,
+        // and we should report any transport protocol not known to be supported by Gradle (but not crash
+        // in case a future Gradle version adds more of them and we're not updated yet to support it).
+    }
     return MavenDependencyVersionsFetcher(
         httpClient = httpClient,
         moduleId = ModuleId(group, name),
@@ -25,10 +35,10 @@ internal operator fun DependencyVersionsFetcher.Companion.invoke(
             // See this issue: https://github.com/jmfayard/refreshVersions/issues/222
             // The error message would be the following:
             // "Authentication scheme 'all'(Authentication) is not supported by protocol 'file'"
-            else -> repository.credentials.username?.let { username ->
+            else -> repository.passwordCredentials?.let { credentials ->
                 Credentials.basic(
-                    username = username,
-                    password = repository.credentials.password ?: return@let null
+                    username = credentials.username ?: return@let null,
+                    password = credentials.password ?: return@let null
                 )
             }
         }
