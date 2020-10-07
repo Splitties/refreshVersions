45f87e80b0dd2d3da2abe10b6bf690549f16d2be
Martin Bonnin <martin@mbonnin.net>
2020-10-07 13:48:03 +0200

    add ApolloGraphQL

---
+++ plugins/dependencies/src/main/kotlin/dependencies/ApolloGraphQL.kt
@@ -0,0 +1,25 @@
+@file:Suppress("PackageDirectoryMismatch", "SpellCheckingInspection", "unused")
+
+import org.gradle.api.Incubating
+
+@Incubating
+object ApolloGraphQL {
+
+    /**
+     * A strongly-typed, caching GraphQL client for the JVM, Android and Kotlin multiplatform.
+     *
+     * GitHub page: [apollographql/apollo-android](https://github.com/apollographql/apollo-android)
+     */
+    const val apolloGradlePlugin = "com.apollographql.apollo:apollo-gradle-plugin:_"
+    const val apolloApi = "com.apollographql.apollo:apollo-api:_"
+    const val apolloRuntime = "com.apollographql.apollo:apollo-runtime:_"
+    const val apolloHttpCache = "com.apollographql.apollo:apollo-http-cache:_"
+    const val apolloNormalizedCache = "com.apollographql.apollo:apollo-normalized-cache:_"
+    const val apolloNormalizedCacheSqlite = "com.apollographql.apollo:apollo-normalized-cache-sqlite:_"
+    const val apolloCoroutinesSupport = "com.apollographql.apollo:apollo-coroutines-support:_"
+    const val apolloRx2Support = "com.apollographql.apollo:apollo-rx2-support:_"
+    const val apolloRx3Support = "com.apollographql.apollo:apollo-rx3-support:_"
+    const val apolloRuntimeKotlin = "com.apollographql.apollo:apollo-runtime-kotlin:_"
+    const val apolloIdlingResource = "com.apollographql.apollo:apollo-idling-resource:_"
+    const val apolloCompiler = "com.apollographql.apollo:apollo-compiler:_"
+}
