372654092852b697cf4e476cb76a091fc1d5846c
Louis CAD <louis.cognault@gmail.com>
2020-10-25 01:15:46 +0200

    Forbid duplicate keys in versions.properties

--- plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/versions/VersionsPropertiesReading.kt
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/versions/VersionsPropertiesReading.kt
@@ -1,5 +1,6 @@
 package de.fayard.refreshVersions.core.internal.versions
 
+import de.fayard.refreshVersions.core.extensions.sequences.uniqueBy
 import de.fayard.refreshVersions.core.extensions.text.substringAfterLastLineStartingWith
 import de.fayard.refreshVersions.core.extensions.text.substringBetween
 
@@ -26,56 +27,72 @@ private fun VersionsPropertiesModel.Companion.readFromTextInternal(
         generatedByVersion = fileContent.substringBetween(generatedByLineStart, "\n")
         sectionsText = fileContent.substringAfterLastLineStartingWith(headerLinesPrefix)
     }
+
+    val sections = mutableListOf<VersionsPropertiesModel.Section>()
+
+    sectionsText.trim().splitToSequence("\n\n").mapNotNull { sectionText ->
+        readSection(sectionText)
+    }.onEach {
+        sections.add(it)
+    }.filterIsInstance<VersionsPropertiesModel.Section.VersionEntry>().uniqueBy(
+        onDuplicate = { key ->
+            throw IllegalArgumentException("The version with key $key has been found twice!")
+        }
+    ) {
+        it.key
+    }.lastOrNull() // Force iteration of the whole sequence.
+
     return VersionsPropertiesModel(
         preHeaderContent = preHeaderContent,
         generatedByVersion = generatedByVersion,
-        sections = sectionsText.trim().splitToSequence("\n\n").mapNotNull { sectionText ->
+        sections = sections
+    )
+}
 
-            val lines = sectionText.lines().map { it.trim() }
+private fun readSection(sectionText: String): VersionsPropertiesModel.Section? {
+    val lines = sectionText.lines().map { it.trim() }
 
-            val versionLineIndex = lines.indexOfFirst {
-                versionKeysPrefixes.any { prefix -> it.startsWith(prefix) }
-            }.also {
-                if (it == -1) {
-                    if (sectionText.isEmpty()) return@mapNotNull null
-                    return@mapNotNull VersionsPropertiesModel.Section.Comment(lines = sectionText)
-                }
+    val versionLineIndex = lines.indexOfFirst {
+        VersionsPropertiesModel.versionKeysPrefixes.any { prefix -> it.startsWith(prefix) }
+    }.also {
+        if (it == -1) {
+            if (sectionText.isEmpty()) return null
+            return VersionsPropertiesModel.Section.Comment(lines = sectionText)
+        }
+    }
+
+    val versionLine = lines[versionLineIndex]
+
+    fun String.isAvailableUpdateComment(): Boolean {
+        return startsWith("##") && "${VersionsPropertiesModel.availableComment}=" in this
+    }
+
+    val remainingLines = lines.subList(
+        fromIndex = (versionLineIndex + 1).coerceAtMost(lines.size),
+        toIndex = lines.size
+    )
+
+    var availableUpdatesSectionPassed = false
+
+    val (availableUpdatesComments, trailingComments) = remainingLines.partition { line ->
+        line.isAvailableUpdateComment().also { isAvailableUpdateLine ->
+            if (isAvailableUpdateLine) check(availableUpdatesSectionPassed.not()) {
+                "Putting custom comments between available updates comments is not supported."
             }
+            availableUpdatesSectionPassed = isAvailableUpdateLine.not()
+        }
+    }
 
-            val versionLine = lines[versionLineIndex]
+    val versionKey = versionLine.substringBefore('=')
 
-            fun String.isAvailableUpdateComment(): Boolean {
-                return startsWith("##") && "$availableComment=" in this
-            }
-
-            val remainingLines = lines.subList(
-                fromIndex = (versionLineIndex + 1).coerceAtMost(lines.size),
-                toIndex = lines.size
-            )
-
-            var availableUpdatesSectionPassed = false
-
-            val (availableUpdatesComments, trailingComments) = remainingLines.partition { line ->
-                line.isAvailableUpdateComment().also { isAvailableUpdateLine ->
-                    if (isAvailableUpdateLine) check(availableUpdatesSectionPassed.not()) {
-                        "Putting custom comments between available updates comments is not supported."
-                    }
-                    availableUpdatesSectionPassed = isAvailableUpdateLine.not()
-                }
-            }
-
-            val versionKey = versionLine.substringBefore('=')
-
-            VersionsPropertiesModel.Section.VersionEntry(
-                leadingCommentLines = lines.subList(fromIndex = 0, toIndex = versionLineIndex),
-                key = versionKey,
-                currentVersion = versionLine.substringAfter('=', missingDelimiterValue = "").ifEmpty {
-                    error("Didn't find the value of the version for the following key: $versionKey")
-                },
-                availableUpdates = availableUpdatesComments.map { it.substringAfter('=') },
-                trailingCommentLines = trailingComments
-            )
-        }.toList()
+    return VersionsPropertiesModel.Section.VersionEntry(
+        leadingCommentLines = lines.subList(fromIndex = 0, toIndex = versionLineIndex),
+        key = versionKey,
+        currentVersion = versionLine.substringAfter('=', missingDelimiterValue = "").ifEmpty {
+            error("Didn't find the value of the version for the following key: $versionKey")
+        },
+        availableUpdates = availableUpdatesComments.map { it.substringAfter('=') },
+        trailingCommentLines = trailingComments
     )
 }
 
