9155ae433b67c61742d56ced5c56827db776ed86
Louis CAD <louis.cognault@gmail.com>
2020-10-25 17:39:10 +0100

    Support updating existing VersionEntry

--- plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/versions/VersionsPropertiesTransform.kt
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/versions/VersionsPropertiesTransform.kt
@@ -3,21 +3,35 @@ package de.fayard.refreshVersions.core.internal.versions
 import de.fayard.refreshVersions.core.internal.versions.VersionsPropertiesModel.Section
 import de.fayard.refreshVersions.core.internal.versions.VersionsPropertiesModel.Section.VersionEntry
 
-
-
 internal operator fun VersionsPropertiesModel.plus(
     newEntry: VersionEntry
 ): VersionsPropertiesModel {
     require(newEntry.leadingCommentLines.isEmpty())
     require(newEntry.trailingCommentLines.isEmpty())
 
-    val targetIndex = sections.indexOfFirst {
-        it is VersionEntry && it.key > newEntry.key
-    }.let { if (it == -1) sections.size else it }
     return this.copy(
-        sections = mutableListOf<Section>().also {
-            it.addAll(sections)
-            it.add(index = targetIndex, element = newEntry)
+        sections = mutableListOf<Section>().also { newList ->
+            newList.addAll(sections)
+
+            val existingEntryWithSameKey: VersionEntry?
+            val targetIndex = run {
+                for (i in sections.indices) {
+                    val element = sections[i]
+                    if (element !is VersionEntry || element.key < newEntry.key) continue
+
+                    existingEntryWithSameKey = if (element.key == newEntry.key) element else null
+                    return@run i
+                }
+                existingEntryWithSameKey = null
+                sections.size
+            }
+
+            when (existingEntryWithSameKey) {
+                null -> newList.add(index = targetIndex, element = newEntry)
+                else -> newList[targetIndex] = existingEntryWithSameKey.copy(
+                    availableUpdates = newEntry.availableUpdates
+                )
+            }
         }
     )
 }
