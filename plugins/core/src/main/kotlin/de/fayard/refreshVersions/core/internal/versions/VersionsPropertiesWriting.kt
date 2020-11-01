097c00fb5533fcbea3eebeca2e84cd713e5d5046
Louis CAD <louis.cognault@gmail.com>
2020-11-01 22:40:26 +0100

    Mark function as internal API

--- plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/versions/VersionsPropertiesWriting.kt
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/versions/VersionsPropertiesWriting.kt
@@ -3,9 +3,8 @@ package de.fayard.refreshVersions.core.internal.versions
 import de.fayard.refreshVersions.core.RefreshVersionsCorePlugin
 import de.fayard.refreshVersions.core.Version
 import de.fayard.refreshVersions.core.extensions.gradle.toModuleIdentifier
+import de.fayard.refreshVersions.core.internal.*
 import de.fayard.refreshVersions.core.internal.DependencyWithVersionCandidates
-import de.fayard.refreshVersions.core.internal.RefreshVersionsConfigHolder
-import de.fayard.refreshVersions.core.internal.getVersionPropertyName
 import de.fayard.refreshVersions.core.internal.isAVersionAlias
 import de.fayard.refreshVersions.core.internal.versions.VersionsPropertiesModel.Companion.availableComment
 import de.fayard.refreshVersions.core.internal.versions.VersionsPropertiesModel.Section.Comment
@@ -13,6 +12,7 @@ import de.fayard.refreshVersions.core.internal.versions.VersionsPropertiesModel.
 import org.gradle.api.artifacts.ExternalDependency
 import java.io.File
 
+@InternalRefreshVersionsApi
 fun writeMissingEntriesInVersionProperties(newEntries: Map<String, ExternalDependency>) {
     VersionsPropertiesModel.update { model ->
         val newSections = newEntries.map { (key, d) ->
