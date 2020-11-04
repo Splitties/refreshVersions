794e1677345d0a4edbc2a9d67a8fd42529e2f92e
nikky <git@nikky.moe>
2020-11-04 12:47:35 +0100

    apply quick return suggestions from jmfayard

--- plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/MavenDependencyVersionsFetcherFile.kt
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/MavenDependencyVersionsFetcherFile.kt
@@ -17,19 +17,21 @@ internal class MavenDependencyVersionsFetcherFile(
         require(repoUrl.endsWith('/'))
     }
 
-    override suspend fun getXmlMetadataOrNull(): String? = try {
-        val targetDir = repoDir.resolve("${moduleId.group!!.replace('.', '/')}/${moduleId.name}")
-        val fileNames: Array<String>? = targetDir.list()
-        fileNames?.filter {
-            it.startsWith("maven-metadata") && it.endsWith(".xml")
-        }?.also {
-            check(it.size <= 1) {
-                "Expected only one maven-metadata xml file but got ${it.size} matching files!"
+    override suspend fun getXmlMetadataOrNull(): String? {
+        return try {
+            val targetDir = repoDir.resolve("${moduleId.group!!.replace('.', '/')}/${moduleId.name}")
+            val fileNames: Array<String> = targetDir.list() ?: return null
+            fileNames.filter {
+                it.startsWith("maven-metadata") && it.endsWith(".xml")
+            }.also {
+                check(it.size <= 1) {
+                    "Expected only one maven-metadata xml file but got ${it.size} matching files!"
+                }
+            }.singleOrNull()?.let { filename ->
+                targetDir.resolve(filename).readText()
             }
-        }?.singleOrNull()?.let { filename ->
-            targetDir.resolve(filename).readText()
+        } catch (e: FileNotFoundException) {
+            null
         }
-    } catch (e: FileNotFoundException) {
-        null
     }
 }
