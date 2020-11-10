9d862c71c4cf871d10770a836a25600f3737d68a
Jean-Michel Fayard <jmfayard@gmail.com>
2020-11-10 09:41:41 +0100

    🏗 Build dev versions with jetpack

--- plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/CurrentlyUsedVersion.kt
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/CurrentlyUsedVersion.kt
@@ -5,6 +5,7 @@ import org.gradle.api.initialization.Settings
 @InternalRefreshVersionsApi
 fun Settings.currentVersionOfRefreshVersions(): String {
     return buildscript.configurations.flatMap { it.dependencies }.single {
-        it.group == "de.fayard.refreshVersions"
+        val useJitpack = "${it.group}:${it.name}" == "com.github.jmfayard:refreshVersions" // See https://gradle.com/s/u6p6roznt4kn6
+        it.group == "de.fayard.refreshVersions" || useJitpack
     }.version!!
 }
