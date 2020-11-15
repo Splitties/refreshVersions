f0ffeff76d04a8e33ee16a4cfa6b8378f15715f0
Louis CAD <louis.cognault@gmail.com>
2020-11-15 13:00:51 +0100

    Make the buildSrcLibs plugin apply refreshVersions

--- plugins/buildSrcLibs/src/main/kotlin/de/fayard/buildSrcLibs/BuildSrcLibsPlugin.kt
+++ plugins/buildSrcLibs/src/main/kotlin/de/fayard/buildSrcLibs/BuildSrcLibsPlugin.kt
@@ -2,11 +2,13 @@ package de.fayard.buildSrcLibs
 
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.register
 
 class BuildSrcLibsPlugin : Plugin<Settings> {
 
     override fun apply(settings: Settings) {
+        settings.apply(plugin = "de.fayard.refreshVersions")
         settings.gradle.rootProject {
             project.tasks.register<BuildSrcLibsTask>(
                 name = "buildSrcLibs"
