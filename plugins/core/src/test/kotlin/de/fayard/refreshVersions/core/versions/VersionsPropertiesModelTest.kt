f56f239984df55863c70e396ff45f28be123ad3e
Louis CAD <louis.cognault@gmail.com>
2021-04-26 23:52:48 +0200

    Extract duplicated test code into functions

--- plugins/core/src/test/kotlin/de/fayard/refreshVersions/core/versions/VersionsPropertiesModelTest.kt
+++ plugins/core/src/test/kotlin/de/fayard/refreshVersions/core/versions/VersionsPropertiesModelTest.kt
@@ -9,6 +9,7 @@ import extensions.junit.mapDynamicTest
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
+import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -18,80 +19,68 @@ class VersionsPropertiesModelTest {
     private val samplesDir = testResources.resolve("versions-properties-samples")
 
     @TestFactory
-    fun `test parsing old format`(): List<DynamicTest> {
-        val samplesDirs = samplesDir.resolve("old-format").listFiles()!!.filter {
-            it.isDirectory
-        }.also { check(it.isNotEmpty()) }
-        return samplesDirs.mapDynamicTest { dir ->
-            val inputFile = dir.resolve("input.properties")
-            val outputFile = dir.resolve("output.properties")
-            val parsedModel = VersionsPropertiesModel.readFromText(inputFile.readText())
-            assertEquals(
-                expected = outputFile.readText(),
-                actual = parsedModel.toText(),
-                message = "Output of model parsed from input should match the expected input!"
-            )
-            val reParsedModel = VersionsPropertiesModel.readFromText(parsedModel.toText())
-            assertEquals(
-                expected = parsedModel,
-                actual = reParsedModel,
-                message = "Model shouldn't change after being written and parsed again!"
-            )
-            assertEquals(
-                expected = parsedModel.toText(),
-                actual = reParsedModel.toText(),
-                message = "File content shouldn't change after being parsed and written again!"
-            )
-        }
+    fun `test parsing old format`() = sampleDirs(
+        dirName = "old-format"
+    ).mapDynamicTest { dir ->
+        val inputFile = dir.resolve("input.properties")
+        val outputFile = dir.resolve("output.properties")
+        val parsedModel = VersionsPropertiesModel.readFromText(inputFile.readText())
+        assertEquals(
+            expected = outputFile.readText(),
+            actual = parsedModel.toText(),
+            message = "Output of model parsed from input should match the expected input!"
+        )
+        val reParsedModel = VersionsPropertiesModel.readFromText(parsedModel.toText())
+        assertEquals(
+            expected = parsedModel,
+            actual = reParsedModel,
+            message = "Model shouldn't change after being written and parsed again!"
+        )
+        assertEquals(
+            expected = parsedModel.toText(),
+            actual = reParsedModel.toText(),
+            message = "File content shouldn't change after being parsed and written again!"
+        )
     }
 
     @TestFactory
-    fun `test new format`(): List<DynamicTest> {
-        val sampleFiles = samplesDir.resolve("new-format").walkTopDown().filter {
-            it.extension == "properties"
-        }.toList().also { check(it.isNotEmpty()) { "No test files found!" } }
-        return sampleFiles.mapDynamicTest {
-            val fileContent = it.readText()
-            val parsedModel = VersionsPropertiesModel.readFromText(fileContent)
-            val reParsedModel = VersionsPropertiesModel.readFromText(parsedModel.toText())
-            assertEquals(
-                expected = fileContent,
-                actual = parsedModel.toText(),
-                message = "Parsing and writing back should yield the same result!"
-            )
-            assertEquals(
-                expected = parsedModel.toText(),
-                actual = reParsedModel.toText(),
-                message = "File content shouldn't change after being parsed and written again!"
-            )
-            assertEquals(
-                expected = parsedModel,
-                actual = reParsedModel,
-                message = "Model shouldn't change after being written and parsed again!"
-            )
-        }
+    fun `test new format`() = sampleFiles(
+        dirName = "new-format"
+    ).mapDynamicTest {
+        val fileContent = it.readText()
+        val parsedModel = VersionsPropertiesModel.readFromText(fileContent)
+        val reParsedModel = VersionsPropertiesModel.readFromText(parsedModel.toText())
+        assertEquals(
+            expected = fileContent,
+            actual = parsedModel.toText(),
+            message = "Parsing and writing back should yield the same result!"
+        )
+        assertEquals(
+            expected = parsedModel.toText(),
+            actual = reParsedModel.toText(),
+            message = "File content shouldn't change after being parsed and written again!"
+        )
+        assertEquals(
+            expected = parsedModel,
+            actual = reParsedModel,
+            message = "Model shouldn't change after being written and parsed again!"
+        )
     }
 
     @TestFactory
-    fun `test new format parsing`(): List<DynamicTest> {
-        val sampleFiles = samplesDir.resolve("new-format-parsing-only").walkTopDown().filter {
-            it.extension == "properties"
-        }.toList().also { check(it.isNotEmpty()) { "No test files found!" } }
-        return sampleFiles.mapDynamicTest {
-            VersionsPropertiesModel.readFromText(it.readText())
-        }
+    fun `test new format parsing`() = sampleFiles(
+        dirName = "new-format-parsing-only"
+    ).mapDynamicTest {
+        VersionsPropertiesModel.readFromText(it.readText())
     }
 
     @TestFactory
-    fun `test parsing incorrectly formatted version files fails as expected`(): List<DynamicTest> {
-        val sampleFiles = samplesDir.resolve("new-format-invalid").walkTopDown().filter {
-            it.extension == "properties"
-        }.toList().also { check(it.isNotEmpty()) { "No test files found!" } }
-        return sampleFiles.mapDynamicTest {
-            val fileContent = it.readText()
-            assertFailsWith<IllegalStateException> {
-                VersionsPropertiesModel.readFromText(fileContent)
-            }
+    fun `test parsing incorrectly formatted version files fails as expected`() = sampleFiles(
+        dirName = "new-format-invalid"
+    ).mapDynamicTest {
+        val fileContent = it.readText()
+        assertFailsWith<IllegalStateException> {
+            VersionsPropertiesModel.readFromText(fileContent)
         }
     }
 
@@ -109,5 +98,17 @@ class VersionsPropertiesModelTest {
             plugin.com.osacky.doctor=0.6.2
             """.trimIndent()
         input.insertNewLinesIfNeeded() shouldBe expected
+    }
+
+    private fun sampleFiles(dirName: String): List<File> {
+        return samplesDir.resolve(dirName).walkTopDown().filter {
+            it.extension == "properties"
+        }.toList().also { check(it.isNotEmpty()) { "No test files found!" } }
+    }
+
+    private fun sampleDirs(dirName: String): List<File> {
+        return samplesDir.resolve(dirName).listFiles()!!.filter {
+            it.isDirectory
+        }.toList().also { check(it.isNotEmpty()) { "No test directories found!" } }
     }
 }
