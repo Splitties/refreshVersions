5176b3406b74ba8060f0867656d10f16db3b7de3
Louis CAD <louis.cognault@gmail.com>
2021-04-26 21:38:16 +0200

    Workaround Gradle classloader issue
    That minor test code refactoring changes the generated class names,
    and changes the owner of the TestEntry local class, getting rid of
    the issue that happened only with Gradle test runner (worked find with
    IntelliJ IDEA test runner).

--- plugins/core/src/test/kotlin/de/fayard/refreshVersions/core/LegacyBootstrapMigratorTest.kt
+++ plugins/core/src/test/kotlin/de/fayard/refreshVersions/core/LegacyBootstrapMigratorTest.kt
@@ -17,61 +17,58 @@ class LegacyBootstrapMigratorTest {
         }!!.asList()
         return sampleDirs.map { dir ->
             DynamicTest.dynamicTest(dir.name) {
-                class TestEntry(
-                    val inputFile: File,
-                    val expectedOutputFile: File
-                )
-                listOf(
-                    TestEntry(
-                        inputFile = dir.resolve("gvy-input.settings.gradle"),
-                        expectedOutputFile = dir.resolve("gvy-output.settings.gradle")
-                    ),
-                    TestEntry(
-                        inputFile = dir.resolve("kt-input.settings.gradle.kts"),
-                        expectedOutputFile = dir.resolve("kt-output.settings.gradle.kts")
-                    )
-                ).flatMap {
-                    listOf(
-                        it,
-                        TestEntry(
-                            inputFile = it.inputFile.resolveSibling(
-                                relative = "buildSrc-${it.inputFile.name}"
-                            ),
-                            expectedOutputFile = it.expectedOutputFile.resolveSibling(
-                                relative = "buildSrc-${it.expectedOutputFile.name}"
-                            )
-                        )
-                    )
-                }.filter {
-                    it.inputFile.exists()
-                }.also { list ->
-                    check(list.isNotEmpty())
-                }.forEach { testEntry ->
-                    `test LegacyBootstrapMigrator`(
-                        inputFile = testEntry.inputFile,
-                        expectedOutputFile = testEntry.expectedOutputFile,
-                        isBuildSrc = testEntry.inputFile.name.startsWith("buildSrc-")
-                    )
-                }
+                `test LegacyBootstrapMigrator`(dir)
             }
         }
     }
 
-    private fun `test LegacyBootstrapMigrator`(
-        inputFile: File,
-        expectedOutputFile: File,
-        isBuildSrc: Boolean
-    ) {
-        val currentVersion = RefreshVersionsCorePlugin.currentVersion
-        val inputText = inputFile.readText().replace("€{currentVersion}", currentVersion)
-        val expectedOutput = expectedOutputFile.readText().replace("€{currentVersion}", currentVersion)
-        val actualOutput = LegacyBootstrapMigrator(
-            isBuildSrc = isBuildSrc,
-            isKotlinDsl = inputFile.extension == "kts"
-        ).gradleSettingsFileContentUpdatedWithPluginsDslSetup(settingsFileContent = inputText)
-        assertEquals(
-            expected = expectedOutput,
-            actual = actualOutput
+    private fun `test LegacyBootstrapMigrator`(dir: File) {
+        class TestEntry(
+            val inputFile: File,
+            val expectedOutputFile: File
         )
+        listOf(
+            TestEntry(
+                inputFile = dir.resolve("gvy-input.settings.gradle"),
+                expectedOutputFile = dir.resolve("gvy-output.settings.gradle")
+            ),
+            TestEntry(
+                inputFile = dir.resolve("kt-input.settings.gradle.kts"),
+                expectedOutputFile = dir.resolve("kt-output.settings.gradle.kts")
+            )
+        ).flatMap {
+            listOf(
+                it,
+                TestEntry(
+                    inputFile = it.inputFile.resolveSibling(
+                        relative = "buildSrc-${it.inputFile.name}"
+                    ),
+                    expectedOutputFile = it.expectedOutputFile.resolveSibling(
+                        relative = "buildSrc-${it.expectedOutputFile.name}"
+                    )
+                )
+            )
+        }.filter {
+            it.inputFile.exists()
+        }.also { list ->
+            check(list.isNotEmpty())
+        }.forEach { testEntry ->
+            val inputFile = testEntry.inputFile
+            val expectedOutputFile = testEntry.expectedOutputFile
+            val isBuildSrc = testEntry.inputFile.name.startsWith("buildSrc-")
+            val currentVersion = RefreshVersionsCorePlugin.currentVersion
+            val inputText = inputFile.readText().replace("€{currentVersion}", currentVersion)
+
+            val expectedOutput = expectedOutputFile.readText().replace("€{currentVersion}", currentVersion)
+            val actualOutput = LegacyBootstrapMigrator(
+                isBuildSrc = isBuildSrc,
+                isKotlinDsl = inputFile.extension == "kts"
+            ).gradleSettingsFileContentUpdatedWithPluginsDslSetup(settingsFileContent = inputText)
+
+            assertEquals(
+                expected = expectedOutput,
+                actual = actualOutput
+            )
+        }
     }
 }
