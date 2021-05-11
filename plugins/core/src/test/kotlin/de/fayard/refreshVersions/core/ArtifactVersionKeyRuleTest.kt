7ed899f47e3889c420ab2495897a53d286afcd63
Louis CAD <louis.cognault@gmail.com>
2021-05-12 01:25:51 +0200

    Add a few version key rules sorting tests

--- plugins/core/src/test/kotlin/de/fayard/refreshVersions/core/ArtifactVersionKeyRuleTest.kt
+++ plugins/core/src/test/kotlin/de/fayard/refreshVersions/core/ArtifactVersionKeyRuleTest.kt
@@ -2,7 +2,11 @@ package de.fayard.refreshVersions.core
 
 import de.fayard.refreshVersions.core.internal.ArtifactVersionKeyRule
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+import testutils.junit.dynamicTest
+import kotlin.test.assertEquals
 
 class ArtifactVersionKeyRuleTest {
 
@@ -28,4 +32,35 @@ class ArtifactVersionKeyRuleTest {
         "org.jetbrains.kotlinx:kotlinx-coroutines-core-native",
         "org.jetbrains.kotlinx:kotlinx-coroutines-core-android"
     )
+
+    @TestFactory
+    fun `Rules are ordered by specificity`(): List<DynamicTest> = listOf(
+        listOf(
+            ArtifactVersionKeyRule(
+                artifactPattern = "  androidx.recyclerview:recyclerview-selection".trimStart(),
+                versionKeyPattern = "^^^^^^^^.             ^^^^^^^^^^^^^^^^^^^^^^"
+            ),
+            ArtifactVersionKeyRule(
+                artifactPattern = "  androidx.*:*".trimStart(),
+                versionKeyPattern = "^^^^^^^^^^"
+            )
+        ),
+        listOf(
+            ArtifactVersionKeyRule(
+                artifactPattern = "  androidx.camera:camera-view(-*)".trimStart(),
+                versionKeyPattern = "^^^^^^^^.       ^^^^^^.^^^^"
+            ),
+            ArtifactVersionKeyRule(
+                artifactPattern = "  androidx.*:*".trimStart(),
+                versionKeyPattern = "^^^^^^^^^^"
+            )
+        )
+    ).map { expectedOrderedList ->
+        dynamicTest(displayName = expectedOrderedList.first().artifactPattern) {
+            assertEquals(
+                expected = expectedOrderedList,
+                actual = expectedOrderedList.sortedDescending()
+            )
+        }
+    }
 }
