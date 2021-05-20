dcdc1793a60ebe4643c7f7882415fa1e65d20169
Jean-Michel Fayard <jmfayard@gmail.com>
2021-05-20 11:20:59 +0200

    Setup snapshots publishing on Sonatype
    Co-Authored-By: Louis CAD <louis.cognault@gmail.com>

---
+++ plugins/buildSrc/src/main/kotlin/Property.kt
@@ -0,0 +1,5 @@
+import org.gradle.api.provider.Property
+
+infix fun <T> Property<T>.by(value: T) {
+    set(value)
+}
--- plugins/buildSrc/src/main/kotlin/extensions/Project.kt
+++
@@ -1,7 +0,0 @@
-package extensions
-
-import org.gradle.api.Project
-
-fun Project.propertyOrEnv(key: String): String? {
-    return findProperty(key) as String? ?: System.getenv(key)
-}
