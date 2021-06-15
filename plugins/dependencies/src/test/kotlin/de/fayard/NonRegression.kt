60bdd3d959b6d991605e4b35e3fb4a2871f71fae
Jean-Michel Fayard <jmfayard@gmail.com>
2021-06-15 16:22:22 +0200

    Test that we do not change version keys

--- plugins/dependencies/src/test/kotlin/de/fayard/NonRegression.kt
+++ plugins/dependencies/src/test/kotlin/de/fayard/NonRegression.kt
@@ -1,5 +1,6 @@
 package de.fayard
 
+import de.fayard.refreshVersions.core.internal.ArtifactVersionKeyReader
 import de.fayard.refreshVersions.internal.DependencyMapping
 import de.fayard.refreshVersions.internal.getArtifactNameToConstantMapping
 import io.kotest.assertions.fail
@@ -8,6 +9,7 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.collections.haveSize
 import io.kotest.matchers.should
+import testutils.isInCi
 import java.io.File
 
 val testResources: File = File(".").absoluteFile.resolve("src/test/resources")
@@ -28,6 +30,32 @@ class NonRegression: FreeSpec({
         }
         receivedProperties.copyTo(existingProperties, overwrite = true)
         receivedProperties.deleteOnExit()
+    }
+
+    "We should not change version keys" {
+        val mainResources: File = File(".").absoluteFile.resolve("src/main/resources")
+        val rulesDir = mainResources.resolve("refreshVersions-rules")
+        val versionKeyReader = ArtifactVersionKeyReader.fromRules(rulesDir.listFiles()!!.map { it.readText() })
+
+        val existingKeys = testResources.resolve("dependencies-versions-key-validated.txt")
+        val receivedKeys = testResources.resolve("dependencies-versions-key-received.txt")
+
+        val existingMapping = existingKeys.readLines().mapNotNull { DependencyMapping.fromLine(it) }
+        val receivedMapping = getArtifactNameToConstantMapping().map {
+            val key = versionKeyReader.readVersionKey(it.group, it.artifact) ?: "NO-RULE"
+            it.copy(constantName = "version.$key")
+        }
+        receivedKeys.writeText(receivedMapping.joinToString(separator = "\n", postfix = "\n"))
+
+        val breakingChanges = existingMapping - receivedMapping
+        withClue("diff -u ${existingKeys.absolutePath}  ${receivedKeys.absolutePath}") {
+            breakingChanges should haveSize(0)
+        }
+        withClue("Changes to $existingKeys must be committed, but I got new entries") {
+            if (isInCi()) (receivedMapping - existingMapping) should haveSize(0)
+        }
+        receivedKeys.copyTo(existingKeys, overwrite = true)
+        receivedKeys.deleteOnExit()
     }
 
     "Dependencies should not be in the `dependencies` package" {
