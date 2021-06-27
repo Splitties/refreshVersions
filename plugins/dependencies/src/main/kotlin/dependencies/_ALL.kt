b6b7fce3a14ce66cadf5dc4a9895a9b17620f337
Louis CAD <louis.cognault@gmail.com>
2021-06-27 20:52:06 +0200

    Add comments to the _ALL.kt file
    Co-authored-by: Jean-Michel Fayard <jmfayard@gmail.com>

--- plugins/dependencies/src/main/kotlin/dependencies/_ALL.kt
+++ plugins/dependencies/src/main/kotlin/dependencies/_ALL.kt
@@ -1,3 +1,5 @@
+// The name of this file starts with an underscore to make it prominent in the file list.
+
 package dependencies
 
 import AndroidX
@@ -16,6 +18,7 @@ import Spring
 import Square
 import Testing
 
+// All top-level objects holding dependency notations shall be added in this list.
 internal val ALL_DEPENDENCIES_NOTATIONS = listOf(
     AndroidX,
     CashApp,
