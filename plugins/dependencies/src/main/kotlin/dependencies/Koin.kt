79458e8faa1acecd84ff6ea645cc3c5af328e765
Jean-Michel Fayard <jmfayard@gmail.com>
2021-08-24 08:32:23 +0200

    Remove un-needed comments

--- plugins/dependencies/src/main/kotlin/dependencies/Koin.kt
+++ plugins/dependencies/src/main/kotlin/dependencies/Koin.kt
@@ -1,9 +1,9 @@
-@file:Suppress("PackageDirectoryMismatch", "SpellCheckingInspection", "unused") // 1
+@file:Suppress("PackageDirectoryMismatch", "SpellCheckingInspection", "unused")
 
 import de.fayard.refreshVersions.core.DependencyGroup
 import org.gradle.api.Incubating
 
-/**                                                             // 2
+/**                                                            
  * A pragmatic lightweight dependency injection framework for Kotlin developers.
  * Koin is a DSL, a light container and a pragmatic API
  *
