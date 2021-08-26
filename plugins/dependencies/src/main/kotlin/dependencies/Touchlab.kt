bdb496f0f7388d282bbdc70ed90f487264245ca1
Brady Aiello <brady@touchlab.co>
2021-08-26 15:49:04 -0700

    [Issue-388] Add Stately
    [Summary]
    Stately is a common KMM library for native threading, and we'd like to
    use it in refreshVersions.
    https://github.com/touchlab/Stately
    How we're using it for safe multithreading on native in KaMP Kit:
    https://github.com/touchlab/KaMPKit/blob/main/shared/src/commonMain/kotlin/co/touchlab/kampkit/ktor/DogApiImpl.kt#L46
    
    [Fix]
    Add Stately under a Touchlab object. We'll want to add the KMP logging
    library Kermit there as well.
    
    [Testing]
    - `./gradlew :refreshVersions:build`
    - confirmed that the coordinates are generated in
      `dependencies-mapping-validated.txt` and
    `bundled-dependencies-validated.txt`.

---
+++ plugins/dependencies/src/main/kotlin/dependencies/Touchlab.kt
@@ -0,0 +1,29 @@
+@file:Suppress("PackageDirectoryMismatch", "SpellCheckingInspection", "unused")
+
+import de.fayard.refreshVersions.core.DependencyGroup
+import de.fayard.refreshVersions.core.DependencyNotationAndGroup
+import org.gradle.api.Incubating
+
+/**
+ * Multithreaded Kotlin Multiplatform Utilities
+ *
+ * - GitHub page: [Stately](https://github.com/touchlab/Stately)
+ * - [GitHub Releases here](https://github.com/touchlab/Stately/releases)
+ */
+@Incubating
+object Touchlab {
+    val stately = Stately
+    object Stately : DependencyGroup(
+        group = "co.touchlab",
+        rawRule = """
+        co.touchlab:stately-*
+                    ^^^^^^^
+    """.trimIndent()
+    ) {
+        val common = module("stately-common")
+        val concurrency = module("stately-concurrency")
+        val isolate = module("stately-isolate")
+        val isoCollections = module("stately-iso-collections")
+    }
+}
+
