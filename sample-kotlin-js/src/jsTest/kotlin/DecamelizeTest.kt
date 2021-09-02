bfdfa390a48163922e01270145e4188262a19c66
Louis CAD <louis.cognault@gmail.com>
2021-09-03 00:19:12 +0200

    Remove unneeded backticks

--- sample-kotlin-js/src/jsTest/kotlin/DecamelizeTest.kt
+++ sample-kotlin-js/src/jsTest/kotlin/DecamelizeTest.kt
@@ -4,7 +4,7 @@ import kotlin.test.Test
 class DecamelizeTest {
 
     @Test
-    fun `split_hello_world`() {
+    fun split_hello_world() {
         decamelize("helloWorld") shouldBe "hello_world"
         decamelize("helloWorld", separator = "-") shouldBe "hello-world"
     }
