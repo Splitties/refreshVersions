a6a0ee777aa8ab763ccb70d5404230388d59418e
Louis CAD <louis.cognault@gmail.com>
2021-10-25 10:25:27 +0200

    Add tests for ReplacementOfRemovedDependencyNotations.kt

---
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/internal/removals_replacement/ReplacementOfRemovedDependencyNotations.kt
@@ -0,0 +1,10 @@
+package de.fayard.refreshVersions.core.internal.removals_replacement
+
+import de.fayard.refreshVersions.core.internal.codeparsing.ProgrammingLanguage
+
+internal fun List<RemovedDependencyNotation>.replaceRemovedDependencyNotationReferencesIfAny(
+    gradleBuildFileContent: String,
+    programmingLanguage: ProgrammingLanguage
+): String? {
+    TODO()
+}
---
+++ plugins/core/src/test/kotlin/de/fayard/refreshVersions/core/removals_replacement/ReplacementOfRemovedDependencyNotationsTest.kt
@@ -0,0 +1,67 @@
+package de.fayard.refreshVersions.core.removals_replacement
+
+import de.fayard.refreshVersions.core.internal.codeparsing.ProgrammingLanguage
+import de.fayard.refreshVersions.core.internal.removals_replacement.RemovedDependencyNotation
+import de.fayard.refreshVersions.core.internal.removals_replacement.parseRemovedDependencyNotationsHistory
+import de.fayard.refreshVersions.core.internal.removals_replacement.replaceRemovedDependencyNotationReferencesIfAny
+import de.fayard.refreshVersions.core.testResources
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import testutils.junit.mapDynamicTest
+import java.io.File
+
+class ReplacementOfRemovedDependencyNotationsTest {
+
+    private val testDataDir = testResources.resolve("removals-replacement")
+    private val defaultHistoryFileContent = testDataDir.resolve("default-revisions-history.md").readText()
+
+    @TestFactory
+    fun `tests for replaceRemovedDependencyNotationReferencesIfAny`(): List<DynamicTest> {
+        val sampleDirs = testDataDir.listFiles { file ->
+            file.isDirectory
+        }!!.asList()
+        return sampleDirs.mapDynamicTest { dir ->
+            `test replaceRemovedDependencyNotationReferencesIfAny`(dir)
+        }
+    }
+
+    private fun `test replaceRemovedDependencyNotationReferencesIfAny`(dir: File) {
+        val currentRevision = dir.name.substringAfter("-rev", missingDelimiterValue = "0").toInt()
+        val revisionsHistoryFileContent = dir.resolve("revisions-history.md").takeIf {
+            it.exists()
+        }?.readText() ?: defaultHistoryFileContent
+        val revisionsHistory = revisionsHistoryFileContent.lineSequence().parseRemovedDependencyNotationsHistory(
+            currentRevision = currentRevision
+        )
+        listOf(
+            "kt-input.build.gradle.kts" to "kt-output.build.gradle.kts",
+            "gvy-input.build.gradle" to "gvy-output.build.gradle"
+        ).mapNotNull { (inputFileName, outputFileName) ->
+            val inputFile = dir.resolve(inputFileName).takeIf { it.exists() }
+            val outputFile = dir.resolve(outputFileName).takeIf { it.exists() }
+            if (inputFile == null) null else inputFile to outputFile
+        }.forEach { (inputFile, outputFile) ->
+            `test replaceRemovedDependencyNotationReferencesIfAny`(
+                revisionsHistory,
+                inputFile = inputFile,
+                outputFile = outputFile
+            )
+        }
+    }
+
+    private fun `test replaceRemovedDependencyNotationReferencesIfAny`(
+        revisionsHistory: List<RemovedDependencyNotation>,
+        inputFile: File,
+        outputFile: File?
+    ) {
+        val output = revisionsHistory.replaceRemovedDependencyNotationReferencesIfAny(
+            gradleBuildFileContent = inputFile.readText(),
+            programmingLanguage = when (inputFile.extension) {
+                "kts" -> ProgrammingLanguage.Kotlin
+                else -> ProgrammingLanguage.Groovy
+            }
+        )
+        output shouldBe outputFile?.readText()
+    }
+}
