6900e33c05160cb8894d3b907d0431999642ba32
Louis CAD <louis.cognault@gmail.com>
2022-01-24 02:25:12 +0100

    Gradle Enterprise 3.8.1

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("com.gradle.enterprise").version("3.8")
-////                        # available:"3.8.1")
+    id("com.gradle.enterprise").version("3.8.1")
     id("de.fayard.refreshVersions") version "0.40.0"
 }
 
