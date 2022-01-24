b1be7bc63de48f542343be6dbab3ecfa212719e9
Louis CAD <louis.cognault@gmail.com>
2022-01-24 02:24:56 +0100

    Use refreshVersions 0.40.0 in refreshVersions

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     id("com.gradle.enterprise").version("3.8")
 ////                        # available:"3.8.1")
-    id("de.fayard.refreshVersions") version "0.30.2"
-////                            # available:"0.40.0"
+    id("de.fayard.refreshVersions") version "0.40.0"
 }
 
 gradleEnterprise {
