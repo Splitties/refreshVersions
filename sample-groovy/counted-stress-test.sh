a36a10e89b34c654a2a383b838b238b7c71d0454
Louis CAD <louis.cognault@gmail.com>
2022-05-30 00:24:38 +0200

    Use counted-stress-test.sh for memory leak check

---
+++ sample-groovy/counted-stress-test.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+is_uint() { case $1 in '' | *[!0-9]* ) return 1;; esac ;}
+
+is_uint "$1"
+if [ $? -eq 1 ]; then
+    runLimit=9223372036854775807 # Long.MAX_VALUE (64 bits)
+else
+    runLimit=$1
+    shift
+fi
+
+runCount=1
+echo "Run number $runCount"
+$@
+while [ $? -eq 0 ] && [ $runCount -lt $runLimit ]; do
+    ((runCount++))
+    echo "Run number $runCount"
+    $@
+done
