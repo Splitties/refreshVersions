60b91c803fa748215bddcb570eb2d6251a2481c9
Louis CAD <louis.cognault@gmail.com>
2023-08-24 02:05:10 +0200

    Remove extra closing parenthesis

--- plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/RefreshVersionsTask.kt
+++ plugins/core/src/main/kotlin/de/fayard/refreshVersions/core/RefreshVersionsTask.kt
@@ -199,7 +199,7 @@ open class RefreshVersionsTask : DefaultTask() {
             logger.warn("Found ${dependenciesWithHardcodedVersions.count()} dependencies that might have hardcoded versions:")
             warnFor.forEach { logger.warn("- $it") }
             if (logger.isInfoEnabled.not()) {
-                logger.warn("- ${dependenciesWithHardcodedVersions.size - warnFor.size} more... (run with --info) to see them all)")
+                logger.warn("- ${dependenciesWithHardcodedVersions.size - warnFor.size} more... (run with --info to see them all)")
             }
             logger.warn("")
             logger.warn(
